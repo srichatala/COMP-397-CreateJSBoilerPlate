e3eb80dca920703c70419dd3abfa9645bab90221
srichatala <sri2vas@gmail.com>
2015-01-26 16:03:05 -0800

    modified gmae.js

---
+++ COMP-397-CreateJSBoilerPlate/Scripts/game.js
@@ -0,0 +1,52 @@
+﻿// CreateJS Boilerplate for COMP397
+// VARIABLES ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+var canvas;
+var stage;
+var helloText;
+var buttonBitmap;
+
+// FUNCTIONS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas); // Parent Object
+    stage.enableMouseOver(20); // Turn on Mouse Over events
+
+    createjs.Ticker.setFPS(60); // Set the frame rate to 60 fps
+    createjs.Ticker.addEventListener("tick", gameLoop);
+
+    main();
+}
+
+// GAMELOOP
+function gameLoop() {
+    stage.update();
+}
+
+function buttonClicked() {
+    helloText.text = "Goodbye!";
+}
+
+function buttonOut() {
+    buttonBitmap.alpha = 1;
+}
+
+function buttonOver() {
+    buttonBitmap.alpha = 0.5;
+}
+
+function main() {
+    // This is where all the work happens
+    helloText = new createjs.Text("Hello World!", "40px Consolas", "#000000");
+    stage.addChild(helloText); // First Child Object that we add to the stage
+
+    // Green Button
+    buttonBitmap = new createjs.Bitmap("assets/images/images.jpg");
+    buttonBitmap.x = 100;
+    buttonBitmap.y = 100;
+    buttonBitmap.addEventListener("click", buttonClicked);
+    buttonBitmap.addEventListener("mouseout", buttonOut);
+    buttonBitmap.addEventListener("mouseover", buttonOver);
+
+    stage.addChild(buttonBitmap);
+}
+//# sourceMappingURL=game.js.map
